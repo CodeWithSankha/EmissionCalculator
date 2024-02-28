8a955dd1b8a2a3be7eb944a3bffe9f7a86cc0c6a
SANKHA RAY <saheb_ray85@yahoo.co.in>
2024-02-28 22:50:54 +0100

    Initial Commit

---
+++ src/main/java/com/sap/assignment/emissioncalculator/EmissionCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.sap.assignment.emissioncalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmissionCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmissionCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sap/assignment/emissioncalculator/EmissioncalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sap.assignment.emissioncalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmissioncalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
