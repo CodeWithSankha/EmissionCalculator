0f7031243cf74b3b9a8e76ce38cdace322bbd8fe
SANKHA RAY <saheb_ray85@yahoo.co.in>
2024-03-03 00:09:35 +0100

    Fix Unit test for EmissionCalculatorRestController

--- src/test/java/com/sap/assignment/emissioncalculator/controller/TestEmissionCalculatorRestController.java
+++ src/test/java/com/sap/assignment/emissioncalculator/controller/TestEmissionCalculatorRestController.java
@@ -1,6 +1,5 @@
 package com.sap.assignment.emissioncalculator.controller;
 
-import com.sap.assignment.emissioncalculator.exceptions.InvalidArgsException;
 import com.sap.assignment.emissioncalculator.service.EmissionCalculatorService;
 import org.junit.Assert;
 import org.junit.Test;
@@ -10,6 +9,8 @@ import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Objects;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest()
@@ -32,26 +33,26 @@ public class TestEmissionCalculatorRestController {
         Assert.assertNotNull(controller.calculate_emission(parameters));
     }
 
-    @Test(expected = InvalidArgsException.class)
+    @Test
     public void testControllerWoStartCity() {
         RestRequestParameters parameters = new RestRequestParameters();
         parameters.endCity = "end_city";
         parameters.transportationMode = "transportation_mode";
-        controller.calculate_emission(parameters);
+        Assert.assertTrue("EndCity", Objects.requireNonNull(controller.calculate_emission(parameters).getBody()).contains("start_city"));
     }
-    @Test(expected = InvalidArgsException.class)
+    @Test
     public void testControllerWoEndCity() {
         RestRequestParameters parameters = new RestRequestParameters();
         parameters.startCity = "start_city";
         parameters.transportationMode = "transportation_mode";
-        controller.calculate_emission(parameters);
+        Assert.assertTrue("EndCity", Objects.requireNonNull(controller.calculate_emission(parameters).getBody()).contains("end_city"));
     }
 
-    @Test(expected = InvalidArgsException.class)
+    @Test
     public void testControllerWoTransportation() {
         RestRequestParameters parameters = new RestRequestParameters();
         parameters.startCity = "start_city";
         parameters.endCity = "end_city";
-        controller.calculate_emission(parameters);
+        Assert.assertTrue("EndCity", Objects.requireNonNull(controller.calculate_emission(parameters).getBody()).contains("transportation_mode"));
     }
 }
