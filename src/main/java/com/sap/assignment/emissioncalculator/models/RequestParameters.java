5688e1212c85c8dd86d347cffde4ce956a0ed8c8
SANKHA RAY <saheb_ray85@yahoo.co.in>
2024-03-03 11:56:22 +0100

    Cleanup Request Parameter record class

--- src/main/java/com/sap/assignment/emissioncalculator/models/RequestParameters.java
+++ src/main/java/com/sap/assignment/emissioncalculator/models/RequestParameters.java
@@ -1,22 +1,7 @@
 package com.sap.assignment.emissioncalculator.models;
 
-import org.springframework.boot.ApplicationArguments;
-
-import java.util.function.Function;
-
 public record RequestParameters(String startCity,
                                 String endCity,
                                 String transportationMode
 ) {
-/*    public Function<ApplicationArguments, String> startCity() {
-        return (args) -> args.getOptionValues("start").get(0);
-    }
-
-    public Function<ApplicationArguments, String> endCity() {
-        return (args) -> args.getOptionValues("end").get(0);
-    }
-
-    public Function<ApplicationArguments, String> transportationMode() {
-        return (args) -> args.getOptionValues("transportation-method").get(0);
-    }*/
 }
